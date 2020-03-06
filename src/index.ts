e6481eac48ef9e2028b3bccd359dd7d4e17873c3
David Sornberger <davids@bytetechnology.co>
2020-03-06 08:28:58 -0800

    Updates to export the connectors as well

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import MoleculerMikroContext from './service.databases/moleculer.mikro.context';
 import DatabaseContextManager from './service.middlewares/database.context';
+import MikroConnector from './service.databases/mikro.connector';
+import DatabaseConnector from './service.databases/database.connector';
 
-export { DatabaseContextManager, MoleculerMikroContext };
+export {
+  DatabaseContextManager,
+  MoleculerMikroContext,
+  MikroConnector,
+  DatabaseConnector
+};
