c4d3d9d3436cbd5a8d777a5bad46e7f0f08d915f
Ujwal Setlur <ujwal.setlur@gmail.com>
2020-03-29 19:55:29 -0700

    Initial mongo support. Need to test transactional support.

--- src/service.databases/moleculer.mikro.context.ts
+++ src/service.databases/moleculer.mikro.context.ts
@@ -1,11 +1,12 @@
 import moleculer from 'moleculer';
 import { EntityManager } from 'mikro-orm';
+import { MongoDriver } from 'mikro-orm/dist/drivers/MongoDriver';
 
 class MoleculerMikroContext<
   P = unknown,
   M extends object = {}
 > extends moleculer.Context<P, M> {
-  public entityManager!: EntityManager;
+  public entityManager!: EntityManager | EntityManager<MongoDriver>;
 }
 
 export default MoleculerMikroContext;
